f8bc51d6a1ac727e43468765e352470909e0a876
hunzzzzz <hunzz.study@gmail.com>
2024-02-14 13:25:24 +0900

    [Setting] 프로젝트 초기 세팅
    - 프로젝트 초기 세팅
    - build.gradle에 필요 라이브러리 설정 및 정리

---
+++ src/main/kotlin/team/b5/moviezip/MovieZipApplication.kt
@@ -0,0 +1,11 @@
+package team.b5.moviezip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieZipApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieZipApplication>(*args)
+}
---
+++ src/test/kotlin/team/b5/moviezip/MovieZipApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.b5.moviezip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieZipApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
