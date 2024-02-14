d00e96cd6da07f417f681e4ffad0507efd619fa5
hunzzzzz <hunzz.study@gmail.com>
2024-02-14 15:00:39 +0900

    [Setting] SwaggerConfig, SecurityConfig 설정 파일 추가
    - build.gradle에 Swagger 관련 라이브러리 추가
    - SwaggerConfig 설정 파일 추가
    - SecurityConfig 설정 파일 추가

---
+++ src/main/kotlin/team/b5/moviezip/global/config/SecurityConfig.kt
@@ -0,0 +1,30 @@
+package team.b5.moviezip.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .cors { it.disable() }
+            .headers { it.frameOptions { foc -> foc.disable() } }
+            .authorizeHttpRequests {
+                it.requestMatchers(
+                    "/h2-console/**",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**"
+                ).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .build()
+    }
+}
