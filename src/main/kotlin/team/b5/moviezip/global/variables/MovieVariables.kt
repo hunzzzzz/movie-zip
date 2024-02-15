67b76cba4eac8ca809da1c685dda5a2ee00aeda7
hunzzzzz <hunzz.study@gmail.com>
2024-02-15 20:41:17 +0900

    [Feat] CSV 파일의 데이터를 읽고 가공하는 로직 구현
    - build.gradle에 opencsv 관련 라이브러리 추가 (CSV 파일을 가공하기 위함)
    - .gitignore에 movie.csv 파일 추가
    - Movie와 Member의 연관관계를 끊어냄 (추후 다시 연결 가능성 O)
    - MemberController와 MemberService에 addMovies 메서드 추가
    - CSV 파일의 국적 데이터를 MovieNation을 전환하기 위해 각각의 값을 key-value로 가지는 map 생성
        - MovieVariables 클래스에 companion object 변수로 movieNationMap 선언 ("한글" to "ENG")

---
+++ src/main/kotlin/team/b5/moviezip/global/variables/MovieVariables.kt
@@ -0,0 +1,7 @@
+package team.b5.moviezip.global.variables
+
+class MovieVariables {
+    companion object {
+        val movieNationMap = mapOf("한국" to "KOREA", "일본" to "JAPAN", "미국" to "USA")
+    }
+}
--- src/main/kotlin/team/b5/moviezip/movie/controller/MovieController.kt
+++ src/main/kotlin/team/b5/moviezip/movie/controller/MovieController.kt
@@ -1,10 +1,17 @@
 package team.b5.moviezip.movie.controller
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.b5.moviezip.movie.service.MovieService
 
 @RestController
+@RequestMapping("/movies")
 class MovieController(
     private val movieService: MovieService
 ) {
+    // 영화 데이터 추가
+    @GetMapping
+    fun addMovies() = ResponseEntity.ok().body(movieService.addMovies())
 }
