2334e304f7696de47410a810203c88b27140beae
hyeonseok <ghostpar12@naver.com>
2024-02-15 12:50:27 +0900

    [Refactor] secret 값과 토큰 위치 변경

--- src/main/kotlin/team/b5/moviezip/global/security/jwt/JwtPlugin.kt
+++ src/main/kotlin/team/b5/moviezip/global/security/jwt/JwtPlugin.kt
@@ -12,23 +12,21 @@ import java.time.Instant
 import java.util.*
 
 @Component
-class JwtPlugin
- {
-    companion object {
-        const val SECRET = "PO4c8z1Hia5gJG3oeuF3MR2BB4Ws4aZ4"
-        const val ISSUER = "team.sparta.com"
-        const val ACCESS_TOKEN_EXPIRATION_HOUR: Long = 168
-    }
+class JwtPlugin(
+    @Value("\${auth.jwt.issuer}") private val issuer: String,
+    @Value("\${auth.jwt.secret}") private val secret: String,
+    @Value("\${auth.jwt.accessTokenExpirationHour}") private val accessTokenExpirationHour: Long,
+) {
     fun validateToken(jwt: String): Result<Jws<Claims>> { // kotlin 에서는 try catch 대신 result 사용
         return kotlin.runCatching {
-            val key = Keys.hmacShaKeyFor(SECRET.toByteArray(StandardCharsets.UTF_8))
+            val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
             // key 로 서명 거증, 만료시간체크
             Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt)
         }
     }
 
     fun generateAccessToken(subject: String, email: String, role: String): String {
-        return generateToken(subject, email, role, Duration.ofHours(ACCESS_TOKEN_EXPIRATION_HOUR))
+        return generateToken(subject, email, role, Duration.ofHours(accessTokenExpirationHour))
     }
 
 
@@ -38,11 +36,11 @@ class JwtPlugin
             .build()
 
         val now = Instant.now()
-        val key = Keys.hmacShaKeyFor(SECRET.toByteArray(StandardCharsets.UTF_8))
+        val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
 
         return Jwts.builder()
             .subject(subject)
-            .issuer(ISSUER)
+            .issuer(issuer)
             .issuedAt(Date.from(now))
             .expiration(Date.from(now.plus(expirationPeriod)))
             .claims(claims)
