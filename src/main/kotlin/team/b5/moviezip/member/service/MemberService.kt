3e9a450850800037b5e5a3ba4b21b61a15a2931f
hyeonseok <ghostpar12@naver.com>
2024-02-15 12:41:59 +0900

    [Feat] jwtPlugin 생성자 추가 및 로그인 기능 구현

--- src/main/kotlin/team/b5/moviezip/member/service/MemberService.kt
+++ src/main/kotlin/team/b5/moviezip/member/service/MemberService.kt
@@ -4,14 +4,18 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import team.b5.moviezip.global.security.jwt.JwtPlugin
+import team.b5.moviezip.member.dto.request.MemberLoginRequest
 import team.b5.moviezip.member.dto.request.MemberRequest
+import team.b5.moviezip.member.dto.response.MemberLoginResponse
 import team.b5.moviezip.member.repository.MemberRepository
 
 @Service
 @Transactional
 class MemberService(
     private val memberRepository: MemberRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtPlugin: JwtPlugin
 ) {
     // 회원가입
     fun signup(memberRequest: MemberRequest) =
@@ -38,7 +42,7 @@ class MemberService(
     private fun validateRequest(memberRequest: MemberRequest, memberId: Long) {
         if (memberRepository.existsByNickname(memberRequest.nickname) && memberRepository.findByNickname(memberRequest.nickname).id != memberId)
             throw Exception("") // TODO
-        else if (memberRepository.existsByEmail(memberRequest.email) && memberRepository.findByEmail(memberRequest.email).id != memberId)
+        else if (memberRepository.existsByEmail(memberRequest.email) && memberRepository.findByEmail(memberRequest.email)?.id != memberId)
             throw Exception("") // TODO
         else if (memberRequest.password != memberRequest.password2) throw Exception("") // TODO
     }
@@ -46,4 +50,25 @@ class MemberService(
     // 회원 조회
     private fun getMember(memberId: Long) =
         memberRepository.findByIdOrNull(memberId) ?: throw Exception("") // TODO
+
+    //로그인
+    fun login(memberLoginRequest: MemberLoginRequest): MemberLoginResponse {
+        val member =
+            memberRepository.findByEmail(memberLoginRequest.email) ?: throw Exception("") // TODO
+        if (!passwordEncoder.matches(
+                memberLoginRequest.password,
+                member.password
+            )
+        ) {
+            throw Exception("") // TODO
+        }
+        return MemberLoginResponse(
+            accessToken = jwtPlugin.generateAccessToken(
+                subject = member.id.toString(),
+                email = member.email,
+                role = member.role.name
+            )
+        )
+    }
 }
+//
