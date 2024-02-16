e584cc11445820d05b1c75ed6a05b9cad581e119
hunzzzzz <hunzz.study@gmail.com>
2024-02-16 09:45:53 +0900

    [Feat] 영화 단건 조회 기능 구현
    - MemberController와 MemberService에 findMovie 메서드 추가
    - MovieResponse 클래스 생성

--- src/main/kotlin/team/b5/moviezip/movie/controller/MovieController.kt
+++ src/main/kotlin/team/b5/moviezip/movie/controller/MovieController.kt
@@ -2,6 +2,7 @@ package team.b5.moviezip.movie.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.b5.moviezip.movie.service.MovieService
@@ -14,4 +15,8 @@ class MovieController(
     // 영화 데이터 추가
     @GetMapping
     fun addMovies() = ResponseEntity.ok().body(movieService.addMovies())
+
+    // 영화 단건 조회
+    @GetMapping("/{movieId}")
+    fun findMovie(@PathVariable movieId: Long) = ResponseEntity.ok().body(movieService.findMovie(movieId))
 }
